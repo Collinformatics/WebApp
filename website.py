74ce3716a07a4377dfa68d9c7ec08360fbb698f4
Collin Andrews <pizzaTrucks@outlook.com>
2025-04-10 22:26:56 -0500

    Add files via upload

--- website.py
+++ website.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template_string
 inTitle = ('Specificity Network Identification via '
            'Positional Entropy based Refinement (SNIPER)')
 
+# width: 420px;
+
 app = Flask(__name__)
 
 
@@ -22,7 +24,7 @@ def home():
             <title>SNIPER</title>
             <style>
                 body {
-                    background-color: {{black}};
+                    background-color: {{ black }};
                     color: #f0f0f0;
                     font-family: Arial, sans-serif;
                     padding: 40px;
@@ -38,45 +40,50 @@ def home():
                     padding: 10px;
                 }
                 p {
-                    font-size: 18px;
+                    font-size: {{ fontSize }}px;
                     text-align: center;
                     padding-top: 5px;
                     padding-bottom: 0px;
                 }
                 .description-container {
-                    background-color: {{grey}};
-                    color: {{white}};
+                    background-color: {{ grey }};
+                    color: {{ white }};
                     font-size: 20px;
-                    margin-top: 30px;
-                    border-radius: 5px;
                     text-align: center;
-                    padding: 5px 20px;
-                    text-align: center;
-                    margin: 20px auto;
+                    border-radius: {{ borderRad }}px;
                     line-height: 1.2;
+                    margin: 20px auto;
+                    padding: 15px 20px;
                 }
-                .upload-container {
-                    background-color: {{grey}};
+
+
+                .container {
+                    background-color: {{ grey }};
+                    font-size: 20px;
+                    text-align: center;
+                    border-radius: {{ borderRad }}px;
                     display: flex;
                     flex-direction: column;
                     align-items: center;
-                    border-radius: 10px;
-                    width: 420px;
-                    margin: 0px auto;
                     box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-                    padding: 30px 50px 30px 50px;
+                    margin: {{ spacer }}px auto;
+                    margin-bottom: {{ marginB }}px;
+                    padding: {{ 30 }}px 50px 30px 50px;
                 }
+
                 .div-container {
-                    background-color: {{grey}};
-                    color: {{white}};
+                    background-color: {{ grey }};
+                    color: {{ white }};
                     font-size: 20px;
-                    width: 420px; /* Added 'px' */
-                    margin: 30px auto; /* Centers it horizontally */
-                    border-radius: 5px;
                     text-align: center;
-                    padding: 20px;
-                }        
-                .upload-header {
+                    border-radius: {{ borderRad }}px;
+                    margin: {{ spacer }}px auto;
+                    margin-bottom: {{ marginB }}px;
+                    padding: {{ 30 }}px 50px 30px 50px;
+                } /* width: 420px; */
+
+
+                .div-header {
                     color: #23FF55;
                     font-size: 20px;
                     font-weight: normal;
@@ -84,44 +91,55 @@ def home():
                     padding-bottom: 15px;
                 }
                 input[type="file"] {
-                    background-color: {{black}};
+                    background-color: {{ black }};
                     color: #101010;
-                    font-size: 18px;
-                    margin-bottom: 20px;
-                    border-radius: 6px;
+                    font-size: {{ fontSize }}px;
+                    margin-bottom: {{ marginB }}px;
+                    border: 2px solid {{ greyDark }};
+                    border-radius: {{ borderRad }}px;
                     width: 100%;
                     max-width: 350px;
-                    border: 2px solid {{greyDark}};
                     padding: 8px;
                 }
-                .input-form {
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;  /* centers the inputs */
-                }
-                .input-form input {
-                    background-color: {{ black }};
-                    color: #f0f0f0;
-                    font-size: 18px;
-                    text-align: center;
-                    margin-bottom: 20px;
-                    border-radius: 6px;
-                    width: 100%;
-                    max-width: 350px;
-                    border: 2px solid #303030;
-                    padding: 10px;
-                }
                 button {
-                    background-color: {{green}};
-                    color: {{white}};
-                    font-size: 18px;
+                    background-color: {{ green }};
+                    color: {{ white }};
+                    font-size: {{ fontSize }}px;
                     cursor: pointer;
                     border: none;
-                    border-radius: 5px;
+                    border-radius: {{ borderRad }}px;
                     padding: 8px 20px;
                 }
                 button:hover {
-                    background-color: {{greenLight}};
+                    background-color: {{ greenLight }};
+                }
+                .input-form {
+                    align-items: center;
+                    text-align: center; /* center form text */
+                    display: flex;
+                    flex-direction: column;
+                    font-size: {{ fontSize }}px;
+                    margin-bottom: 12px;
+                }
+                .form-group {
+                    width: 100%;
+                    max-width: 350px;
+                    margin-bottom: 12px;
+                }
+                .form-group label {
+                    display: block;
+                    margin-bottom: {{ spacerMini }}px;
+                    font-size: {{ fontSize }}px;
+                    text-align: center;
+                }
+                .form-group input {
+                    font-size: {{ fontSize }}px;
+                    border-radius: {{ borderRad }}px;
+                    border: 2px solid {{ greyDark }};
+                    text-align: center;
+                    width: 100%;
+                    margin-bottom: {{ spacer }}px;
+                    padding: 8px;
                 }
             </style>
         </head>
@@ -135,36 +153,47 @@ def home():
                 <p>{{ ct3|safe }}</p>
                 <p>{{ ct4|safe }}</p>
             </div>
-            <form class="upload-container" method="POST" 
+            <form class="container" method="POST" 
                     action="/upload" enctype="multipart/form-data">
-                <div class="upload-header">Upload FASTA File:</div>
+                <div class="div-header">Upload FASTA File:</div>
                 <input type="file" name="fastaFile" accept=".fasta,.fa" required>
                 <button type="submit">Upload</button>
             </form>
-            <div class="div-container">
-            
-            
+            <div class="container">
                 <form action="/run" method="POST" class="input-form">
-                    <label for="enzymeName">Enzyme Name:</label><br>
-                    <input type="text" id="enzymeName" name="enzymeName" required><br><br>
+                    <div class="div-header">Experiment Parameters:</div>
+                    <div class="form-group">
+                        <label for="enzymeName">Enzyme Name:</label>
+                        <input type="text" id="enzymeName" name="enzymeName" required>
+                    </div>
 
-                    <label for="threshold">Entropy Threshold:</label><br>
-                    <input type="number" id="threshold" name="threshold" step="0.1" required><br><br>
-            
-                    <label for="topN">Top N Substrates:</label><br>
-                    <input type="number" id="topN" name="topN" required><br><br>
+                    <div class="form-group">
+                        <label for="threshold">Entropy Threshold:</label>
+                        <input type="number" id="threshold" name="threshold" 
+                        step="0.1" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="topN">Select Number of Substrates:</label>
+                        <input type="number" id="topN" name="topN" required>
+                    </div>
             
                     <button type="submit">Evaluate</button>
                 </form>
-                
-                
-            </div>
+</div>
         </body>
     </html>
     ''',
     black='#151515', white='FFFFFF',
     grey='#303030', greyDark='#202020',
-    green='#23FF55', greenLight='#25EC58',
+    green='#23FF55', greenLight='#1DE64A',
+
+    spacer=20, spacerMini=5,
+    padSide=20, padTB=20,
+    marginB=12,
+    fontSize=18,
+    borderRad=5,
+
 
     title=inTitle,
 
